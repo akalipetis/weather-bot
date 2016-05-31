42ff4e37a7f15d8da020c42931a863cfc29217ce
Antonis Kalipetis <akalipetis@gmail.com>
2016-05-31 17:34:20 +0300

    Integrate LUIS

--- app.js
+++ app.js
@@ -11,27 +11,21 @@ var server = restify.createServer();
 var iframeUrl = 'https://webchat.botframework.com/embed/' + process.env.APP_ID + '?s=' +
     process.env.APP_IFRAME_SECRET;
 
-var weatherBot = new builder.BotConnectorBot();
-weatherBot.add('/', new builder.CommandDialog()
-    .matches('^hello', function (session) {
-        session.send('Hi there!');
-    })
-    .matches('^weather', function (session) {
-    	session.beginDialog('/weather');
-    })
-    .onDefault(function (session) {
-        session.send('I didn\'t understand. Say hello to me!');
-    })
-);
+var weatherBot = new builder.BotConnectorBot(),
+    luisDialog = new builder.LuisDialog(process.env.LUIS_URL);
+weatherBot.add('/', luisDialog);
 
-weatherBot.add('/weather', [
-    function(session) {
-        builder.Prompts.text(session, 'In which city dear?');
-    },
-    function(session, results) {
-        var city = results.response,
-            url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + process.env.API_KEY,
+luisDialog.on('WeatherQuery', [
+    function (session, args, next) {
+        var city = builder.EntityRecognizer.findEntity(args.entities, 'city'),
+            url,
             parsedBody;
+        if (!city) {
+            city = 'Athens';
+        } else {
+            city = city.entity;
+        }
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + process.env.API_KEY;
         request(url, function(error, response, body) {
             if (error !== null) {
                 session.send('Something went wrong 😞 "%s"', error);
@@ -41,13 +35,15 @@ weatherBot.add('/weather', [
                 session.send('⚠️ - I got an awkward status code: "%d"', response.statusCode);
             }
             parsedBody = JSON.parse(body);
-            session.send('There\'s %s in %s. The temperature currently is %s.',
-                         parsedBody.weather[0].main, 'Athens',
-                         (parsedBody.main.temp + kelvin).toFixed(2));
+            session.send('There\'s %s in %s. The temperature currently is %s °C.',
+                         parsedBody.weather[0].description, city,
+                         (parsedBody.main.temp + kelvin).toFixed(0));
             session.endDialog();
         });
-    }
+    },
 ]);
+
+luisDialog.onDefault(builder.DialogAction.send('I\'m sorry. I didn\'t understand.'));
 
 server.get('/', function indexHTML(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
